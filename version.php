94c0c94aeddbcdfdabcb18ffebda5245ca398af0
LawrenceZhou <hmskyijun@gmail.com>
2016-04-26 00:37:07 +0800

    permission trial2

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_widget';
-$plugin->version = 201604242233;
+$plugin->version = 201604260036;
 $plugin->release = 'v1.0';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_ALPHA;

03c9761e5755b480c37ff17a2bdf6bad8da660a9
LawrenceZhou <hmskyijun@gmail.com>
2016-04-27 12:02:50 +0800

    add server info and the number of cameras 5.

--- mod_form.php
+++ mod_form.php
@@ -69,8 +69,8 @@ class mod_widget_mod_form extends moodleform_mod {
 
 //to be edited
         $mform->addElement('date_time_selector', 'livestarttime', get_string('livestarttime', 'widget'));
-        $cameranumber_options(1 => '1', 2 => '2', 3 => '3');
-        $mform->addElement('select', 'cameranumber', get_string('cameranumber', 'widget'), $cameranumber_options);
+        //$cameranumber_options(1 => '1', 2 => '2', 3 => '3');
+        //$mform->addElement('select', 'cameranumber', get_string('cameranumber', 'widget'), $cameranumber_options);
         $mform->addElement('text', 'kurentoaddress', get_string('kurentoaddress', 'widget'), array('size' => '64'));
 
         // Adding the rest of widget settings, spreading all them into this fieldset

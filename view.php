888f329265692a7515147eba0de215bb7484c66d
LawrenceZhou <hmskyijun@gmail.com>
2016-05-14 10:40:46 +0800

    add kurento 6.

--- view.php
+++ view.php
@@ -94,61 +94,10 @@ echo $OUTPUT->box($intro_string.$date_string.$end_string);
 
 if (has_capability('mod/widget:createvideo', $context)) {
     $camera_number_db = $widget->cameranumber;
-    //for ($i = 1; $i <= $camera_number_db; $i++) {
-    //echo '<div><button name="camera'.$i.'" onclick=M.mod_widget.create_camera("video'.$i.'")>Click to choose the camera</button></div>';
-    //echo '<video id="video'.$i.'" width="487" height="365"></video>';    
-    //}
-    echo    '<meta charset="utf-8">
-    <meta http-equiv="cache-control" content="no-cache">
-    <meta http-equiv="pragma" content="no-cache">
-    <meta http-equiv="expires" content="0">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="img/kurento.png" type="image/png" />
-
-    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="bower_components/demo-console/index.css">
-    <link rel="stylesheet" href="bower_components/ekko-lightbox/dist/ekko-lightbox.min.css">
-    <link rel="stylesheet" href="css/kurento.css">
-
-    <script src="bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="bower_components/demo-console/index.js"></script>
-    <script src="bower_components/ekko-lightbox/dist/ekko-lightbox.min.js"></script>
-    <script src="bower_components/ekko-lightbox/dist/ekko-lightbox.min.js"></script>
-    <script src="bower_components/adapter.js/adapter.js"></script>
-
-    <script src="bower_components/kurento-client/js/kurento-client.js"></script>
-    <script src="bower_components/kurento-utils/js/kurento-utils.js"></script>
-
-    <script src="js/index.js"></script>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-5">
-          <h3>Local stream</h3>
-          <video id="videoInput" autoplay width="480px" height="360px" poster="img/webrtc.png"></video>
-        </div>
-        <div class="col-md-2">
-          <a id="start" href="#" class="btn btn-success">
-            <span class="glyphicon glyphicon-play"></span> Start</a>
-          <br/>
-          <br/>
-          <a id="stop" href="#" class="btn btn-danger">
-            <span class="glyphicon glyphicon-stop"></span> Stop</a>
-        </div>
-        <div class="col-md-5">
-          <h3>Remote stream</h3>
-          <video id="videoOutput" autoplay width="480px" height="360px" poster="img/webrtc.png"></video>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <label class="control-label" for="console">Console</label><br><br>
-          <div id="console" class="democonsole">
-            <ul></ul>
-          </div>
-        </div>
-      </div>
-    </div>';
+    for ($i = 1; $i <= $camera_number_db; $i++) {
+    echo '<div><button name="camera'.$i.'" onclick=M.mod_widget.create_camera("video'.$i.'")>Click to choose the camera</button></div>';
+    echo '<video id="video'.$i.'" width="487" height="365"></video>';    
+    }
 }else {
     //
 }

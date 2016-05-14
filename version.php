29b64f4c43c5bb9d2359cd952130366b5c0d80b9
LawrenceZhou <hmskyijun@gmail.com>
2016-05-14 11:15:42 +0800

    add kurento 9.

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_widget';
-$plugin->version = 201605141107;
+$plugin->version = 201605141115;
 $plugin->release = 'v1.0';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_ALPHA;
